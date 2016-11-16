988334b1bf1ff479dac67344956be8d8b583eb65
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-17 10:27:40 +1300

    Refactored some JS

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 $(document).ready(function () {
+var initDisplay = 0;
+var numbers = [];
+//
+// var display = function(x){
+//   if (x != "*" || x != "/" || x != "+" || x != "-"){
+//     numbers.push(x);
+//     $("#display").html(numbers);
+//   }
+  // else if (x != "*" || x != "/" || x != "+" || x != "-" && ("#display").html === true){
+  //    $("#display").html( $("#display").html + x);
+  // }
+  // else $("#display").html(x);
+// }
 
-var display = function(x){
-  $("#display").html(x);
+$(".button").click(function(){
+  var buttonValue = $(this).attr("value");
+  // if (buttonValue === "*" || buttonValue === "/" || buttonValue === "+" || buttonValue === "-"){
+  //   $("#display").html(buttonValue);}
+  if (buttonValue === "AC"){
+    numbers = [];
+    $("#display").html(initDisplay);
+  }
+
+  else if (buttonValue === "CE"){
+    numbers.pop();
+    // if (numbers.length = 0){
+    //   $("#display").html(initDisplay)
+    // }
+    // else $("#display").html(numbers);
+  }
+
+  else if (buttonValue != "="){
+    numbers.push(buttonValue);
+    $("#display").html(numbers);
+  }
+
+  else {
+    $("#display").html(eval(numbers.join("")));
+    numbers = [];
+    // numbers.push = (eval(numbers.join("")))};
 }
 
-$("#button").click(function(){
-  alert("clicked "+$(this).attr("value"));
-  display($(this).attr("value"));
+  // if (buttonValue === "*" || buttonValue === "/" || buttonValue === "+" || buttonValue === "-"){
+  //   display($(this).attr("value"));}
+  // else display($(this).attr("value"));
 })
 
 })
