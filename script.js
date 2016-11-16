e64261ed6b639952720a956dc856b494483f783a
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-16 16:21:17 +1300

    Add jQuery click function

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-// var operation = [];
-// //
-// // document.addEventListener("click", getButton);
-//
-// var test = document.getElementById("calculator");
-// console.log(test);
+var operation = [];
 
 
-var reply_click = function()
-{
-    alert("Button clicked, id "+this.id+", text"+this.innerHTML);
-}
-document.getElementById('AC').onclick = reply_click;
-document.getElementById('CE').onclick = reply_click;
-document.getElementById('3').onclick = reply_click;
 
-//
-// function whatClicked(evt) {
-//     alert(evt.target.id);
-// }
-//
-// test.addEventListener("click", whatClicked, false);
-//
-// function getButton() {
-// return
-// }
+$(document).ready(function () {
+
+$( "#7" ).click(function() {
+  operation.push(7);
+  console.log(operation);
+});
+$( "#8" ).click(function() {
+  operation.push(8);
+  console.log(operation);
+});
+$( "#addition" ).click(function() {
+  operation.push("+");
+  console.log(operation);
+});
+$( "#equal" ).click(function() {
+  var result = [operation.join("")];
+  console.log(result);
+  return result;
+});
+
+});
