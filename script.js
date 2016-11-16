dedd988865f2d91f849b22e00eee9a12f818a6ea
Mischa Saunders <mischa.saunders@gmail.com>
2016-11-16 15:42:40 +1300

    try with a button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var operation = [];
+// var operation = [];
+// //
+// // document.addEventListener("click", getButton);
 //
-// document.addEventListener("click", getButton);
+// var test = document.getElementById("calculator");
+// console.log(test);
 
-var test = document.getElementById("calculator");
-console.log(test);
+
+var reply_click = function()
+{
+    alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+}
+document.getElementById('AC').onclick = reply_click;
+document.getElementById('CE').onclick = reply_click;
+document.getElementById('3').onclick = reply_click;
+
 //
 // function whatClicked(evt) {
 //     alert(evt.target.id);
